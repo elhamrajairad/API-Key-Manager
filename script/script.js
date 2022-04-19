24d37ca7796a174bd69efe2f12a069b5cbd1110e
elham rajaee <elham.rajairad@gmail.com>
2022-04-19 20:23:14 +0430

    add button delete and edite

--- script/script.js
+++ script/script.js
@@ -10,22 +10,38 @@ const formData = function () {
   let tdTagUserName = document.createElement("td");
   let tdTagApiKey = document.createElement("td");
   let tdTagApiSecret = document.createElement("td");
+  let deleteBtn = document.createElement("a");
+  let editeBtn = document.createElement("a");
 
   tdTagNumber.textContent += 1;
   tdTagUserName.textContent = inputUserName.value;
   tdTagApiKey.textContent = inputApiKey.value;
   tdTagApiSecret.textContent = inputApiSecret.value;
+  deleteBtn.textContent = "Delete";
+  editeBtn.textContent = "Edite";
+
+  editeBtn.classList.add("btn");
+  editeBtn.classList.add("btnEdite");
+  deleteBtn.classList.add("btn");
+  deleteBtn.classList.add("btnDelete");
 
   trTag.appendChild(tdTagNumber);
   trTag.appendChild(tdTagUserName);
   trTag.appendChild(tdTagApiKey);
   trTag.appendChild(tdTagApiSecret);
+  trTag.appendChild(editeBtn);
+  trTag.appendChild(deleteBtn);
   bodyTable.appendChild(trTag);
 };
-
+let formReset = function () {
+  inputUserName.value = "";
+  inputApiSecret.value = "";
+  inputApiKey.value = "";
+};
 document
   .querySelector(".form__api-fields")
   .addEventListener("submit", function (e) {
     e.preventDefault();
     formData();
+    formReset();
   });
